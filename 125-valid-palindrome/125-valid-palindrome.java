c8d6d9230566d22c720807688c7d89c159ce9c53
Utkarsh Singh <47549380+U7K4R5H@users.noreply.github.com>
2022-04-02 12:48:52 +0530

    Time: 23 ms (34.78%), Space: 46.8 MB (27.61%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.java
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -1,1 +1,16 @@
-class Solution { public boolean isPalindrome(String s) { s = s.toLowerCase(); s = s.replaceAll("[^a-z0-9]",""); int l=0, r=s.length()-1; while(l<=r){ if(s.charAt(l)==s.charAt(r)) { l++; r--; } else { return false; } } return true; } }
+class Solution {
+  public boolean isPalindrome(String s) {
+    s = s.toLowerCase();
+    s = s.replaceAll("[^a-z0-9]", "");
+    int l = 0, r = s.length() - 1;
+    while (l <= r) {
+      if (s.charAt(l) == s.charAt(r)) {
+        l++;
+        r--;
+      } else {
+        return false;
+      }
+    }
+    return true;
+  }
+}
