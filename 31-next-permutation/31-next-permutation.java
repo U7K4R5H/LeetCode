7c4137be6dd2e76463b724bcec4723b82e62a229
Utkarsh Singh <47549380+U7K4R5H@users.noreply.github.com>
2022-04-03 13:09:11 +0530

    Time: 1 ms (85.00%), Space: 43.4 MB (73.48%) - LeetHub

--- 31-next-permutation/31-next-permutation.java
+++ 31-next-permutation/31-next-permutation.java
@@ -30,26 +30,56 @@
 } */
 class Solution {
     public void nextPermutation(int[] nums) {
+        /*
+            \U0001d470\U0001d48f\U0001d495\U0001d496\U0001d48a\U0001d495\U0001d48a\U0001d490\U0001d48f: This is an observation-based problem you need to take 
+            some examples and try out some examples.
+
+            \U0001d468\U0001d491\U0001d491\U0001d493\U0001d490\U0001d482\U0001d484\U0001d489: 
+            1. Start traversing the array backward's from len-2 index and find 
+            index of such an element whose value is smaller than it's right digit. 
+            Let's call this index as left.
+            2. If the above index is -1(Array is already sorted) then reverse it. 
+            this is an edge case.
+            3. Else again traverse the array backwords and find the index(right) 
+            of an element which is greater then element at left index.
+            4. Swap the elements at left and right index positions.
+            5. Finally reverse the array from left + 1 index to the end.
+
+            \U0001d47b\U0001d48a\U0001d48e\U0001d486 \U0001d46a\U0001d490\U0001d48e\U0001d491\U0001d48d\U0001d486\U0001d499\U0001d48a\U0001d495\U0001d49a: This algorithm will take linear time and constant space.
+        */
+        if(nums == null || nums.length <= 1)
+            return;
         
-        if(nums == null || nums.length <= 1) return;
-      int i = nums.length - 2;
-      while(i >= 0 && nums[i] >= nums[i + 1]) i--; 
-      if(i >= 0) {                          
-        int j = nums.length - 1;           
-        while(nums[j] <= nums[i]) j--;           
-        swap(nums, i, j);                     
+        int len = nums.length;
+        int left = len - 2;
+        
+        while(left >= 0 && nums[left] >= nums[left+1])
+            left--;
+        
+        if(left >= 0){
+            int right = len - 1;
+            while(right >= 0 && nums[left] >= nums[right])
+                right--;
+            
+            swap(nums, left, right);
+        }
+        
+        reverse(nums, left+1);
     }
-    reverse(nums, i + 1, nums.length - 1);       
-}
+    
+    public static void swap(int[] nums, int a, int b) {
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
+    }
 
-public void swap(int[] nums, int i, int j) {
-    int temp = nums[i];
-    nums[i] = nums[j];
-    nums[j] = temp;
-}
-
-public void reverse(int[] nums, int i, int j) {
-    while(i < j) 
-      swap(nums, i++, j--);
-}
+    public static void reverse(int[] nums, int low) {
+        int left = low;
+        int right = nums.length - 1;
+        while (left < right) {
+            swap(nums, left, right);
+            left++;
+            right--;
+        }
+    }
 }
