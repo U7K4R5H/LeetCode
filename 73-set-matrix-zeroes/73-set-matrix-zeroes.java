17504124ad76779a9f86b5b9c5a0ac727a27da11
Utkarsh Singh <47549380+U7K4R5H@users.noreply.github.com>
2022-04-04 07:39:42 +0530

    Time: 0 ms (100.00%), Space: 43.3 MB (99.67%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -1,4 +1,5 @@
 class Solution {
+    /*
     public void setZeroes(int[][] matrix) {
         int[] row = new int[matrix.length];
         Arrays.fill(row,-1);
@@ -21,4 +22,27 @@ class Solution {
             }
         }
     }
+    */
+    
+     public void setZeroes(int[][] matrix) {
+      		int col0=1, rows=matrix.length, cols=matrix[0].length;
+		for(int i=0; i<rows; i++) {
+			if(matrix[i][0] == 0) {
+				col0=0;
+			}
+			for(int j=1; j<cols; j++) {
+				if(matrix[i][j] == 0) {
+					matrix[i][0] = matrix[0][j] = 0;
+				}
+			}
+		}
+		//Traverse Matrix in reverse order i.e from down right to top left
+		
+		for (int i = rows - 1; i >= 0; i--) {
+            for (int j = cols - 1; j >= 1; j--)
+                if (matrix[i][0] == 0 || matrix[0][j] == 0)
+                    matrix[i][j] = 0;
+            if (col0 == 0) matrix[i][0] = 0;
+        }
+     }
 }
