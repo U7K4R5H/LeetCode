89dbfd7f7e7ba237fd4b340cf202bf924d679d26
Utkarsh Singh <47549380+U7K4R5H@users.noreply.github.com>
2022-04-05 09:02:33 +0530

    Time: 1 ms (35.01%), Space: 42.3 MB (69.12%) - LeetHub

--- 75-sort-colors/75-sort-colors.java
+++ 75-sort-colors/75-sort-colors.java
@@ -1,22 +1,53 @@
+// class Solution {
+//     public void sortColors(int[] nums) {
+//         //Arrays.sort(nums);
+//         int res[] = new int[3];
+//         for(int i=0 ;i<nums.length; i++) {
+//             res[nums[i]]++;
+//         }
+//         int x = 0;
+//         for(int i=0; i<res[0]; i++) {
+//             nums[x] = 0;
+//             x++;
+//         }
+//         for(int i=0; i<res[1]; i++) {
+//             nums[x] = 1;
+//             x++;
+//         }
+//         for(int i=0; i<res[2]; i++) {
+//             nums[x] = 2;
+//             x++;
+//         }
+//     }
+// }
+
 class Solution {
     public void sortColors(int[] nums) {
-        //Arrays.sort(nums);
-        int res[] = new int[3];
-        for(int i=0 ;i<nums.length; i++) {
-            res[nums[i]]++;
-        }
-        int x = 0;
-        for(int i=0; i<res[0]; i++) {
-            nums[x] = 0;
-            x++;
-        }
-        for(int i=0; i<res[1]; i++) {
-            nums[x] = 1;
-            x++;
-        }
-        for(int i=0; i<res[2]; i++) {
-            nums[x] = 2;
-            x++;
+        int lo = 0; 
+        int hi = nums.length - 1; 
+        int mid = 0; 
+        int temp; 
+        while (mid <= hi) { 
+            switch (nums[mid]) { 
+                case 0: { 
+                    temp = nums[lo]; 
+                    nums[lo] = nums[mid]; 
+                    nums[mid] = temp; 
+                    lo++; 
+                    mid++; 
+                    break; 
+                } 
+                case 1: 
+                    mid++; 
+                    break; 
+                case 2: { 
+                    temp = nums[mid]; 
+                    nums[mid] = nums[hi]; 
+                    nums[hi] = temp; 
+                    hi--; 
+                    break; 
+                } 
+            } 
         }
     }
 }
