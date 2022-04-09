4810dcb66d6e0f69bd938b75d8e5b75f264a2724
Utkarsh Singh <47549380+U7K4R5H@users.noreply.github.com>
2022-04-09 23:05:42 +0530

    Time: 15 ms (8.10%), Space: 44.2 MB (6.77%) - LeetHub

--- 2138-divide-a-string-into-groups-of-size-k/2138-divide-a-string-into-groups-of-size-k.java
+++ 2138-divide-a-string-into-groups-of-size-k/2138-divide-a-string-into-groups-of-size-k.java
@@ -1,32 +1,20 @@
 class Solution {
-public static String[] divideString(String s, int k, char fill) {
+    public static String[] divideString(String s, int k, char fill) {
         ArrayList<String> ans = new ArrayList<>();
-
-        for(int i=0; i<s.length() && i+k<s.length()+1 ; i += k) {
-
-            String str = s.substring(i,i+k);
-            ans.add(str);
-        }
-
-        int d = 0;
-
-        if(s.length()%k == 0) {
-            d = 0;
-        }
-        else {
-            d = k - s.length() % k;
-            int p = s.length() - s.length()%k;
-
-            String last ="";
-            for(int i=p; i<s.length(); i++) {
-                last += s.charAt(i);
+        String res = "";
+        for(int i=0; i<s.length(); i++) {
+            res += s.charAt(i);
+            if(res.length() == k){
+                ans.add(res);
+                res = "";
             }
-            for(int x=1; x<=d; x++) {
-                last = last + fill;
-            }
-            ans.add(last);
         }
-
+        if(res.length() > 0 && res.length() != k) {
+            while(res.length() != k) {
+                res += fill;
+            }
+            ans.add(res);
+        }
         String[] r=new String[ans.size()];
         for(int i=0;i<ans.size();i++){
             r[i]=ans.get(i);
@@ -34,3 +22,40 @@ public static String[] divideString(String s, int k, char fill) {
         return r;
     }
 }
+
+// class Solution {
+// public static String[] divideString(String s, int k, char fill) {
+//         ArrayList<String> ans = new ArrayList<>();
+
+//         for(int i=0; i<s.length() && i+k<s.length()+1 ; i += k) {
+
+//             String str = s.substring(i,i+k);
+//             ans.add(str);
+//         }
+
+//         int d = 0;
+
+//         if(s.length()%k == 0) {
+//             d = 0;
+//         }
+//         else {
+//             d = k - s.length() % k;
+//             int p = s.length() - s.length()%k;
+
+//             String last ="";
+//             for(int i=p; i<s.length(); i++) {
+//                 last += s.charAt(i);
+//             }
+//             for(int x=1; x<=d; x++) {
+//                 last = last + fill;
+//             }
+//             ans.add(last);
+//         }
+
+//         String[] r=new String[ans.size()];
+//         for(int i=0;i<ans.size();i++){
+//             r[i]=ans.get(i);
+//         }
+//         return r;
+//     }
+// }
