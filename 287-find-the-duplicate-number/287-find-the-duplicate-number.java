7a5798a18adb0e155d4f5456477bfdc113986036
Utkarsh Singh <47549380+U7K4R5H@users.noreply.github.com>
2022-04-13 16:35:46 +0530

    Time: 5 ms (83.93%), Space: 60.1 MB (84.17%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.java
@@ -1,13 +1,35 @@
 class Solution {
-    public int findDuplicate(int[] nums) {
-        for(int i=0; i<nums.length; i++) {
-            if(nums[Math.abs(nums[i]) - 1] > 0) {
-                nums[Math.abs(nums[i]) - 1] = -nums[Math.abs(nums[i]) - 1];
-            }
-            else {
-                return Math.abs(nums[i]);
-            }
+    // public int findDuplicate(int[] nums) {
+    //     for(int i=0;i<nums.length;i++){
+    //         int current=Math.abs(nums[i]);
+    //             if(nums[current]<0){
+    //                 return current;
+    //                  }
+    //         nums[current]*=-1;
+    //         }
+    //     return -1;
+    // }
+    
+        public int findDuplicate(int[] nums) {
+        
+        // Find the intersection point of the two runners.
+        int tortoise = nums[0];
+        int hare = nums[0];
+        
+        do {
+            tortoise = nums[tortoise];
+            hare = nums[nums[hare]];
+        } while (tortoise != hare);
+
+        // Find the "entrance" to the cycle.
+        tortoise = nums[0];
+        
+        while (tortoise != hare) {
+            tortoise = nums[tortoise];
+            hare = nums[hare];
         }
-        return -1;
+
+        return hare;
     }
+    
 }
