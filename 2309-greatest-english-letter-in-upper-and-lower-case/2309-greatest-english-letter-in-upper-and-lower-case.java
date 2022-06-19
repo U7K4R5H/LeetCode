92ac4fcb9de6cbf69bcd856097f799010f27797d
Utkarsh Singh <47549380+U7K4R5H@users.noreply.github.com>
2022-06-19 09:51:37 +0530

    Time: 11 ms (50.00%), Space: 44.4 MB (25.00%) - LeetHub

--- 2309-greatest-english-letter-in-upper-and-lower-case/2309-greatest-english-letter-in-upper-and-lower-case.java
+++ 2309-greatest-english-letter-in-upper-and-lower-case/2309-greatest-english-letter-in-upper-and-lower-case.java
@@ -46,3 +46,45 @@ class Solution {
         return false;
     }
 }
+
+/* 2nd approach Using HashSet
+class Solution
+{
+    public String greatestLetter(String s)
+    {
+        Set<Character> set = new HashSet<>();
+        for(char ch : s.toCharArray())
+            set.add(ch);
+        
+        for(char ch = 'z'; ch >= 'a'; ch--)
+            if(set.contains(ch) && set.contains((char)('A'+(ch-'a'))))
+               return ""+(char)('A'+(ch-'a'));
+        return "";
+    }
+}
+*/
+
+/* 3rd approach using Counting sort in O(n) time complexity
+class Solution
+{
+    public String greatestLetter(String s) {
+        int[] up = new int[26];
+        int[] low = new int[26];
+        String ans="";
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)>='A' && s.charAt(i)<='Z'){
+                up[s.charAt(i)-'A']=1;
+            }else{
+                low[s.charAt(i)-'a']=1;
+            }
+        }
+        for(int i=25;i>=0;i--){
+            if(up[i]>0 && low[i]>0){
+                ans += (char)(i+'A');
+                break;
+            }
+        }
+        return ans;
+    }
+}
+*/
