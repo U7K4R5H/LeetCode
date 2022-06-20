df51fb8a7867afe2048f49fc5ae9d8e05e245ecb
Utkarsh Singh <47549380+U7K4R5H@users.noreply.github.com>
2022-06-20 09:23:31 +0530

    Time: 2 ms (72.37%), Space: 44.7 MB (45.04%) - LeetHub

---
+++ 46-permutations/46-permutations.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        recurPermute(0, nums, ans);
+        return ans;
+    }
+    private void swap(int i, int j, int[] nums) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    private void recurPermute(int index, int[] nums, List<List<Integer>> ans) {
+        if(index == nums.length) {
+            List<Integer> ds = new ArrayList<>();
+            for(int i : nums) {
+                ds.add(i);
+            }
+            ans.add(new ArrayList<>(ds));
+            return;
+        }
+        for(int i=index; i<nums.length; i++) {
+            swap(i, index, nums);
+            recurPermute(index+1, nums, ans);
+            swap(i, index, nums);
+        }
+    }
+}
