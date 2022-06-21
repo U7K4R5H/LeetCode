92cfbb20cdb7c60684707ec9d462d9947821b29d
Utkarsh Singh <47549380+U7K4R5H@users.noreply.github.com>
2022-06-21 19:45:52 +0530

    Time: 0 ms (100.00%), Space: 43.4 MB (8.77%) - LeetHub

--- 48-rotate-image/48-rotate-image.java
+++ 48-rotate-image/48-rotate-image.java
@@ -3,6 +3,7 @@ class Solution {
         int temp = 0;
         int len = matrix.length;
         
+        //1. Transpose of a 2D Matrix
         for(int i=0; i<len; i++) {
             for(int j=i+1; j<len; j++) {
                 temp = matrix[i][j];
@@ -10,7 +11,7 @@ class Solution {
                 matrix[j][i] = temp;
             }
         }
-        
+        //2. reversing the each row of the Matrix
         for(int i=0; i<len; i++) {
             for(int j=0; j<len/2; j++) {
                 temp = matrix[i][j];
