7f6a36b651e47d12f05ab3f27ca0b561e86ee2d9
Utkarsh Singh <47549380+U7K4R5H@users.noreply.github.com>
2022-06-23 00:11:27 +0530

    Time: 1 ms (34.23%), Space: 43 MB (38.57%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.java
+++ 88-merge-sorted-array/88-merge-sorted-array.java
@@ -1,5 +1,5 @@
 class Solution {
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
+ /*   public void merge(int[] nums1, int m, int[] nums2, int n) {
         int i = 0;
         int j = 0;
         while( i<m && j<n ) {
@@ -18,6 +18,30 @@ class Solution {
         }
         for(int x=0; x<n; x++) {
             nums1[m+x] = nums2[x];
+        }
+    } */
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        if(n != 0 && m != 0) {
+    
+        for(int i=0; i<m; i++) {
+            if(nums1[i] > nums2[0]) {
+                swap(nums1, nums2, i, 0);
+                int curr = nums2[0];
+                int k;
+                for(k=1; k<n && nums2[k]<curr; k++) {
+                    nums2[k - 1] = nums2[k];
+                }
+                nums2[k - 1] = curr;
+            }
+        }
+         for(int x=0; x<n; x++) {
+            nums1[m+x] = nums2[x];
+        }
+        }
+        if(m == 0 && n != 0) {
+            for(int x=0; x<n; x++) {
+                nums1[m+x] = nums2[x];
+            }
         }
     }
     public void swap(int a[], int b[], int i, int j) {
