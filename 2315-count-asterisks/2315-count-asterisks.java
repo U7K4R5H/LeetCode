d2c1ad0c440c2c94ed43549f0242faf9d78cb2e8
Utkarsh Singh <47549380+U7K4R5H@users.noreply.github.com>
2022-06-25 21:57:24 +0530

    Time: 4 ms (14.29%), Space: 41.9 MB (85.71%) - LeetHub

--- 2315-count-asterisks/2315-count-asterisks.java
+++ 2315-count-asterisks/2315-count-asterisks.java
@@ -38,22 +38,39 @@ SC:- Q(N)
 
 Optimized Solution Below
 */
+
+// class Solution {
+//     /** Algorithm:
+//      *  1. Iterate over each character of string and keep a state boolean to determine if you're in a pair or outside pair
+//      *  2. If current char is asterisk and outside pair, then increment the general counter
+//      */
+//     public int countAsterisks(String s) {
+//         boolean pairMode = false;
+//         int asterisks = 0;
+//         for (char c : s.toCharArray()) {
+//             if (c == '|') {
+//                 pairMode = !pairMode;
+//             } else if (c == '*') {
+//                 asterisks += (pairMode ? 0 : 1);
+//             }
+//         }
+//         return asterisks;
+//     }
+// }
+
+
+ //Ultra Pro MAx Solution
 class Solution {
-    /** Algorithm:
-     *  1. Iterate over each character of string and keep a state boolean to determine if you're in a pair or outside pair
-     *  2. If current char is asterisk and outside pair, then increment the general counter
-     */
     public int countAsterisks(String s) {
-        boolean pairMode = false;
-        int asterisks = 0;
-        for (char c : s.toCharArray()) {
-            if (c == '|') {
-                pairMode = !pairMode;
-            } else if (c == '*') {
-                asterisks += (pairMode ? 0 : 1);
-            }
+        int res = 0, bars = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            if (s.charAt(i) == '*' && bars % 2 == 0)
+                res++;
+            if (s.charAt(i) == '|')
+                bars++;
         }
-        return asterisks;
+        return res;
     }
 }
+    
 
