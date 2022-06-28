4b671c57179a8bc38532f6c175f721bfdb223923
Utkarsh Singh <47549380+U7K4R5H@users.noreply.github.com>
2022-06-28 11:26:16 +0530

    Time: 23 ms (12.58%), Space: 56.1 MB (50.58%) - LeetHub

--- 169-majority-element/169-majority-element.java
+++ 169-majority-element/169-majority-element.java
@@ -1,4 +1,4 @@
-
+/* Brute force
 class Solution {
     public int majorityElement(int[] nums) {
         int majorityCount = nums.length / 2;
@@ -16,5 +16,24 @@ class Solution {
             }
         }
         return -1;
+    }
+} */
+
+class Solution {
+    public int majorityElement(int[] nums) {
+        int res = 0;
+        Map<Integer, Integer> hm = new HashMap<Integer, Integer>();
+        for(int num : nums) {
+            if(!hm.containsKey(num)) {
+                hm.put(num, 1);
+            } else {
+                hm.put(num, hm.get(num) + 1);
+            }
+            if(hm.get(num) > nums.length / 2) {
+                res = num;
+                break;
+            }
+        }
+        return res;
     }
 }
