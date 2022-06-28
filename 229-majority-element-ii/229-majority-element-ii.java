96efe2e69b606fcde2c84209addd94fbe1551798
Utkarsh Singh <47549380+U7K4R5H@users.noreply.github.com>
2022-06-28 14:38:23 +0530

    Time: 4 ms (59.39%), Space: 49.5 MB (62.98%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.java
+++ 229-majority-element-ii/229-majority-element-ii.java
@@ -1,17 +1,66 @@
+/* HashMap nlogn N
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
         List<Integer> res = new ArrayList<Integer>();
         Map<Integer, Integer> hm = new HashMap<Integer, Integer>();
         for(int num : nums) {
-            if(!hm.containsKey(num)) {
-                hm.put(num, 1);
-            } else {
-                hm.put(num, hm.get(num) + 1);
-            }
+            hm.put(arr[i], mp.getOrDefault(arr[i], 0) + 1);
             if( (hm.get(num) > nums.length / 3) && (!res.contains(num)) ) {
                 res.add(num);
             }
         }
         return res;
+    }
+} */
+
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        
+        List<Integer> ans = new ArrayList<Integer>();
+        
+        int num1 = Integer.MAX_VALUE, num2 = Integer.MIN_VALUE, count1 = 0, count2 = 0, len = nums.length;
+        
+        for(int num : nums) {
+            
+            if(num == num1){
+                count1++;
+            }
+            else if(num == num2) {
+                count2++;
+            }
+            else if(count1 == 0) {
+                num1 = num;
+                count1 = 1;
+            }
+            else if(count2 == 0) {
+                num2 = num;
+                count2 = 1;
+            }
+            else {
+                count1--;
+                count2--;
+            }
+        }
+        
+        count1 = 0;
+        count2 = 0;
+        
+        for(int num : nums) {
+            if(num == num1) {
+                count1++;
+            }
+            if(num == num2) {
+                count2++;
+            }
+        }
+        
+        if(count1 > len / 3) {
+            ans.add(num1);
+        }
+        if(count2 > len / 3) {
+            ans.add(num2);
+        }
+        
+        return ans;
     }
 }
