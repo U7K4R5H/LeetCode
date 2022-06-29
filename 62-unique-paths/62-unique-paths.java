e30aa934191dce5b724ab9c43aed6275e8d97a84
Utkarsh Singh <47549380+U7K4R5H@users.noreply.github.com>
2022-06-29 19:38:42 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (55.90%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,9 +1,12 @@
+// Recursion with Memoization TopDown approach
 class Solution {
     public int uniquePaths(int m, int n) {
+        
         int[][]dp=new int[m][n];
-        for(int[] dp1:dp){
+        
+        for(int[] dp1:dp)
             Arrays.fill(dp1,-1);
-        }
+        
         return countPaths(0, 0, m, n, dp);
     }
     public int countPaths(int i, int j, int m, int n, int[][] dp) {
@@ -12,11 +15,8 @@ class Solution {
         
         else if (i >= m || j >= n) return 0;
         
-        else if(dp[i][j] != -1) {
-            return dp[i][j];
-        }
-        else {
-            return dp[i][j] = countPaths(i + 1, j, m, n, dp) + countPaths(i, j + 1, m, n, dp);
-        }
+        else if(dp[i][j] != -1) return dp[i][j];
+        
+        else return dp[i][j] = countPaths(i + 1, j, m, n, dp) + countPaths(i, j + 1, m, n, dp);
     }
 }
