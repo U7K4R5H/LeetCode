b2fee248b5616c858ce539fb6c54fbfbc7768b5a
Utkarsh Singh <47549380+U7K4R5H@users.noreply.github.com>
2022-07-02 19:53:13 +0530

    Time: 71 ms (5.24%), Space: 61.7 MB (6.75%) - LeetHub

---
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxArea(int h, int w, int[] horizontalCuts, int[] verticalCuts) {
+        long ans = 1L * (long)getMax(h, horizontalCuts) * (long)getMax(w, verticalCuts);
+        return (int)(ans % 1000000007);
+    }
+    public int getMax(int x, int[] arr) {
+        sort(arr);
+        int n =arr.length;
+        int max = Math.max(arr[0], x - arr[n - 1]);
+        for(int i=1; i<n; i++)
+            max = Math.max(max, arr[i] - arr[i - 1]);
+        return max;
+    }
+    public void sort(int[] arr) {
+        //because Arrays.sort() uses quicksort which is dumb
+        //Collections.sort() uses merge sort
+        ArrayList<Integer> ls = new ArrayList<Integer>();
+        for(int x: arr)
+            ls.add(x);
+        Collections.sort(ls);
+        for(int i=0; i < arr.length; i++)
+            arr[i] = ls.get(i);
+    }
+}
